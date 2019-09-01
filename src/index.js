e5ce5297f9289de72414bb6d3ec9f4aa45f5aba3
Marc Vilchez <ht.m4rck@gmail.com>
2019-09-01 01:15:55 -0500

    test heroku

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const socketio = require('socket.io');
+
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio.listen(server);
+
+app.set('port', process.env.PORT || 3000);
